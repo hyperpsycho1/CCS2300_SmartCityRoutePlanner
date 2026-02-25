10a136078497c1cca9cc526277571b678f542676
DileepaLOQ <DileepaLOQ@192.168.1.11>
2026-02-26 00:53:25 +0530

    Added LocationNode class for BST implementation

--- CCS2300_SmartCityRoutePlanner/src/tree/LocationNode.java
+++ CCS2300_SmartCityRoutePlanner/src/tree/LocationNode.java
@@ -1,5 +1,20 @@
 package tree;
 
+/*
+ * LocationNode class
+ * This represents a single node in the Binary Search Tree (BST).
+ * Each node stores a location name and references to left and right children.
+ */
 public class LocationNode {
 
+    String locationName;        // Name of the location
+    LocationNode left;         // Left child
+    LocationNode right;      // Right child
+
+    // Constructor to initialize node with location name
+    public LocationNode(String locationName) {
+        this.locationName = locationName;
+        this.left = null;
+        this.right = null;
+    }
 }
